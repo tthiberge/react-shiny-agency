e9d2c140dda3d7e6a2b71833e147301da8be5ab0
Theophile Thiberge <tthiberge@gmail.com>
2023-11-01 11:59:51 +0100

    Set up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Home from './pages/Home/';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
