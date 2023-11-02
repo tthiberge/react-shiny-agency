34111b4d5c62c4404e48cf96e5d39005a4238d7e
Theophile Thiberge <tthiberge@gmail.com>
2023-11-02 13:57:46 +0100

    style components

--- src/index.jsx
+++ src/index.jsx
@@ -7,10 +7,19 @@ import Results from './pages/Results'
 import Freelances from './pages/Freelances'
 import Header from './components/Header'
 import Error from './components/Error'
+import createGlobalStyle from 'styled-components'
+
+
+const GlobalStyle = createGlobalStyle`
+    div {
+        font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+`
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <GlobalStyle />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
