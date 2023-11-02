c48e82a4e8c5e10d1341f4362fe324d0060b1389
Theophile Thiberge <tthiberge@gmail.com>
2023-11-02 09:33:28 +0100

    installing router

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,20 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import "./index.css"
-// import App from './App';
-import Home from "./pages/Home"
-import reportWebVitals from "./reportWebVitals"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/'
+import Survey from './pages/Survey/'
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey" element={<Survey />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
+document.getElementById('root')
 )
 
 // If you want to start measuring performance in your app, pass a function
