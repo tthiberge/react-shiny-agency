ddeded5230746e9c3d3bea5250a34e3c1c80e6a3
Theophile Thiberge <tthiberge@gmail.com>
2023-11-02 15:18:55 +0100

    global style

--- src/index.jsx
+++ src/index.jsx
@@ -11,9 +11,13 @@ import createGlobalStyle from 'styled-components'
 
 
 const GlobalStyle = createGlobalStyle`
-    div {
-        font-family: 'Trebuchet MS', Helvetica, sans-serif;
-    }
+  * {
+    font-family: 'Trebuchet MS', Helvetica, sans-serif;
+  }
+
+  body {
+    margin: 0;
+  }
 `
 
 ReactDOM.render(
